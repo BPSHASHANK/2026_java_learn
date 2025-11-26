85dc2ce1a138308a195e15a3f8144600a77b4307
B P SHASHANK <154719115+BPSHASHANK@users.noreply.github.com>
2025-11-26 08:36:09 +0530

    Add files via upload

---
+++ DemoArrayList.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class DemoArrayList {
+
+	public static void main(String[] args) {
+		ArrayList Mylist=new ArrayList();
+		//if we want store homojinous element
+		//ArrayList<INteger>name=new ArrayList<INTEHER>
+		// TODO Auto-generated method stub
+		
+		
+		//operation on ArrayList
+		//add
+		Mylist.add(100);
+		Mylist.add("shashank");
+		
+		Mylist.add(45);
+		Mylist.add(100);
+		Mylist.add(10.7);
+		Mylist.add(100);
+		System.out.println(Mylist);
+		
+		
+		//Removing one element from the ArrayList
+		System.out.println(Mylist.remove(1));
+		
+		//inserting 
+		System.out.println(Mylist.add("ram"));
+		
+		//modify
+		System.out.println(Mylist.set(2, "java"));
+		//retrive teh specific data from the rray List
+		System.out.println(Mylist.get(2));
+
+	}
+
+}
