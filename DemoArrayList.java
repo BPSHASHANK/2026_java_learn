744b11e328ff6788c123e93ebdeb6bd088bfbbf9
B P SHASHANK <154719115+BPSHASHANK@users.noreply.github.com>
2025-11-28 07:52:28 +0530

    Add files via upload

--- DemoArrayList.java
+++ DemoArrayList.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 
+
 public class DemoArrayList {
 
 	public static void main(String[] args) {
@@ -31,6 +32,35 @@ public class DemoArrayList {
 		System.out.println(Mylist.set(2, "java"));
 		//retrive teh specific data from the rray List
 		System.out.println(Mylist.get(2));
+		//Accessing the methods
+		
+//		//method one by for loop
+//		for(int i=0;i<=Mylist.size();i++) {
+//			System.out.println(Mylist.get(i));
+//			
+//		
+//			
+//		}
+		
+		//for each method
+//		for (Object x:Mylist) {
+//			System.out.println(x);
+//		}
+		
+//		another method
+//		//Iteation method ###############################
+//		Iterator a=Mylist.iterator();
+//		while(Mylist.hasNext)){
+//			Sysyem.out.println(it.next());
+//		}
+		ArrayList v=new ArrayList();
+		v.add(100);
+		v.add("Shashank");
+		Mylist.removeAll(v);
+		System.out.println((v));
+		System.out.println("after remove"+Mylist);
+		
+//		
 
 	}
 
